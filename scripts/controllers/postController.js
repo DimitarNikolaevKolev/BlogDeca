aa666730f849f883dd65a808ef6e1499564f5c0f
dimitarnik <dimitarnik89@gmail.com>
2016-09-02 19:31:31 +0300

    SUM

--- scripts/controllers/postController.js
+++ scripts/controllers/postController.js
@@ -3,14 +3,22 @@ class PostController{
         this._postView = postView;
         this._requester = requester;
         this._appKey = appkey;
-        this._baseServiceUrl = baseUrl; //TODO
+        this._baseServiceUrl = baseUrl + "/appdata/" + appkey + "/posts"; //TODO
     }
 
-    createNewPost(){
-        
-    }
+
     
-    showCreatePostPage(){
-        this._postView.showCreatePostPage();
+    showCreatePostPage(fullName,isLoggedIn){
+        this._postView.showCreatePostPage(fullName, isLoggedIn);
+    }
+
+    createNewPost(data){
+            this._requester.post(this._baseServiceUrl,data,function (responseData) {
+                showPopup('success', 'Made new post!');
+                redirectUrl('#/');
+            },    function (responseData) {
+                    showPopup('error', 'Something missing!');
+                }
+            )
     }
 }
